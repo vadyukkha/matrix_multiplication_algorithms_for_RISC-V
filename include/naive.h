43352134fc5102ef2bacb9d8707c2bb90287881f
vadim <144333283+vadyukkha@users.noreply.github.com>
2024-10-10 11:30:34 +0300

    Initial commit

---
+++ include/naive.h
@@ -0,0 +1,16 @@
+// Copyright 2024 by Contributors
+#pragma once
+
+#ifndef INCLUDE_NAIVE
+#define INCLUDE_NAIVE
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <time.h>
+
+void matmul_naive(const float *a, const float *b, float *c, size_t n);
+
+#include "../src/naive.c"
+
+#endif // INCLUDE_NAIVE
