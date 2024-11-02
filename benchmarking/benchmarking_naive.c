fb35f2c6146baf5b4db69d702915779fdb9f8148
danya <reactive722@gmail.com>
2024-11-03 00:23:33 +0300

    #fix comparator in benchmarking_naive.c; #refactor benchmarking_naive.c: n replaced with matSize, testsCount var created; #edit output in benchmark

--- benchmarking/benchmarking_naive.c
+++ benchmarking/benchmarking_naive.c
@@ -6,25 +6,26 @@
 
 void matmul_naive(const int *a, const int *b, int *c, size_t row_a, size_t col_a, size_t col_b);
 
-clock_t compare(const clock_t *a, const clock_t *b) { return (*(clock_t *)a - *(clock_t *)b); }
+int compare(const void *a, const void *b) { return (*(clock_t *)a - *(clock_t *)b); }
 
 int main() {
-    int n = 5;
-    clock_t *results = (clock_t *)malloc(50 * sizeof(clock_t));
-    int *a = (int *)malloc(n * n * sizeof(int));
-    int *b = (int *)malloc(n * n * sizeof(int));
-    int *c = (int *)calloc(n * n, sizeof(int));
-    for (int i = 1; i <= n * n; i++) {
+    int matSize = 100;
+    int testsCount = 100;
+    clock_t *results = (clock_t *)malloc(testsCount * sizeof(clock_t));
+    int *a = (int *)malloc(matSize * matSize * sizeof(int));
+    int *b = (int *)malloc(matSize * matSize * sizeof(int));
+    int *c = (int *)calloc(matSize * matSize, sizeof(int));
+    for (int i = 1; i <= matSize * matSize; i++) {
         a[i - 1] = i + 5;
         b[i - 1] = i + 3;
     }
-    for (int i = 0; i < 50; i++) {
+    for (int i = 0; i < testsCount; i++) {
         clock_t start = clock();
-        // matmul_naive(a, b, c, n, n, n);
+        matmul_naive(a, b, c, matSize, matSize, matSize);
         clock_t end = clock();
         results[i] = end - start;
     }
-    // qsort(results, 50, sizeof(clock_t), compare);
-    printf("%d ", (results[24] + results[25]) / 2);
+    qsort(results, testsCount, sizeof(clock_t), compare);
+    printf("%.1f ", (results[testsCount / 2 - 1] + results[testsCount / 2]) / 2.0);
     return 0;
 }
