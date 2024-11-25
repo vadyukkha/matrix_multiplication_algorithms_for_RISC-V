832f1f4f2cc219f742cd98b0aa439b0215d6d241
vadim <144333283+vadyukkha@users.noreply.github.com>
2024-11-25 22:09:52 +0300

    #build add system for riscv

---
+++ cmake/toolchain-riscv.cmake
@@ -0,0 +1,12 @@
+set(CMAKE_SYSTEM_NAME Linux) 
+set(CMAKE_SYSTEM_PROCESSOR riscv64)
+
+# Toolchain setting
+# set(CMAKE_C_COMPILER riscv64-linux-gnu-gcc)
+# set(CMAKE_CXX_COMPILER riscv64-linux-gnu-g++)
+# set(CMAKE_LINKER riscv64-linux-gnu-ld)
+
+# Toolchain setting
+set(CMAKE_C_COMPILER riscv64-unknown-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER riscv64-unknown-linux-gnu-g++)
+set(CMAKE_LINKER riscv64-unknown-linux-gnu-ld)
