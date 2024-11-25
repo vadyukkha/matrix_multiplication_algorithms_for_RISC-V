12262248344c8ad7e5c773ed6c6b23ca09b18845
vadim <144333283+vadyukkha@users.noreply.github.com>
2024-11-25 22:28:20 +0300

    #fix creating graph

--- benchmarking/benchmarking_transpose.c
+++ benchmarking/benchmarking_transpose.c
@@ -1,4 +1,6 @@
+#include <errno.h>
 #include <stdio.h>
+#include <sys/stat.h>
 #include <time.h>
 
 #include "benchmarking.h"
@@ -6,17 +8,26 @@
 
 #define TEST_COUNT 10
 #define BUFFER_SIZE 256
-#define FILE_PATH "benchmarking_outputs/matmul_transpose.txt"
+#define FILE_PATH "../benchmarking/benchmarking_outputs/matmul_transpose.txt"
 
 void matmul_transpose(const int *a, const int *b, int *c, size_t row_a, size_t col_a, size_t col_b);
 
 int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        printf("Using: %s <matrix_size>\n", argv[0]);
+    if (argc < 3) {
+        printf("Using: %s <step> %s <finish>\n", argv[0], argv[1]);
+        return 1;
+    }
+
+    if (mkdir("../benchmarking/benchmarking_outputs", 0777) != 0 && errno != EEXIST) {
+        perror("Error creating dir");
         return 1;
     }
 
     FILE *file = fopen(FILE_PATH, "w");
+    if (file == NULL) {
+        printf("Error: file was not opened");
+        return 1;
+    }
 
     size_t step = (size_t)atoi(argv[1]);
     size_t finish = (size_t)atoi(argv[2]);
--- benchmarking/benchmarking_vectorization.c
+++ benchmarking/benchmarking_vectorization.c
@@ -1,4 +1,6 @@
+#include <errno.h>
 #include <stdio.h>
+#include <sys/stat.h>
 #include <time.h>
 
 #include "benchmarking.h"
@@ -6,18 +8,27 @@
 
 #define TEST_COUNT 10
 #define BUFFER_SIZE 256
-#define FILE_PATH "benchmarking_outputs/matmul_vectorization.txt"
+#define FILE_PATH "../benchmarking/benchmarking_outputs/matmul_vectorization.txt"
 
 void matmul_vectorization(const int *a, const int *b, int *c, size_t row_a, size_t col_a,
                           size_t col_b);
 
 int main(int argc, char *argv[]) {
     if (argc < 3) {
-        printf("Using: <step> <finish>\n");
+        printf("Using: %s <step> %s <finish>\n", argv[0], argv[1]);
+        return 1;
+    }
+
+    if (mkdir("../benchmarking/benchmarking_outputs", 0777) != 0 && errno != EEXIST) {
+        perror("Error creating dir");
         return 1;
     }
 
     FILE *file = fopen(FILE_PATH, "w");
+    if (file == NULL) {
+        printf("Error: file was not opened");
+        return 1;
+    }
 
     size_t step = (size_t)atoi(argv[1]);
     size_t finish = (size_t)atoi(argv[2]);
