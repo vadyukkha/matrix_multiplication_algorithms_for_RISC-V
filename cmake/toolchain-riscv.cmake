a38f7fa79432b10083a36cc825d9580844c0effc
danya <reactive722@gmail.com>
2025-04-13 13:03:16 +0300

    #fix building of matmul_rvv.s on x86 architecture

--- cmake/toolchain-riscv.cmake
+++ cmake/toolchain-riscv.cmake
@@ -3,6 +3,7 @@ set(CMAKE_SYSTEM_PROCESSOR riscv64)
 
 # Toolchain setting
 add_definitions(-DRISCV)
+set(RISCV ON)
 message(STATUS "Compiling for RISC-V")
 set(CMAKE_C_COMPILER riscv64-unknown-linux-gnu-gcc)
 set(CMAKE_CXX_COMPILER riscv64-unknown-linux-gnu-g++)
